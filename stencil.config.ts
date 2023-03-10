38d9d8269ef3e549129f5c365d51b91786cfe9af
jhuenges <jan.huenges@data4life.care>
2023-03-10 10:11:04 +0100

    Upsi

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,28 +1,27 @@
-import { Config } from "@stencil/core";
-import nodePolyfills from "rollup-plugin-node-polyfills";
+import { Config } from '@stencil/core';
+import nodePolyfills from 'rollup-plugin-node-polyfills';
 
 // https://stenciljs.com/docs/config
 
 export const config: Config = {
-  globalStyle: "src/global/app.css",
-  globalScript: "src/global/app.ts",
-  taskQueue: "async",
+  globalStyle: 'src/global/app.css',
+  globalScript: 'src/global/app.ts',
+  taskQueue: 'async',
   rollupPlugins: {
     after: [nodePolyfills()],
   },
   outputTargets: [
     {
-      type: "www",
+      type: 'www',
       copy: [
         {
-          src: "../node_modules/sql.js/dist/sql-wasm.wasm",
-          dest: "assets/sql-wasm.wasm",
+          src: '../node_modules/sql.js/dist/sql-wasm.wasm',
+          dest: 'assets/sql-wasm.wasm',
         },
-        { src: "config.js", dest: "config.js" },
       ],
       // comment the following line to disable service workers in production
       serviceWorker: null,
-      baseUrl: "https://myapp.local/",
+      baseUrl: 'https://myapp.local/',
     },
   ],
 };
